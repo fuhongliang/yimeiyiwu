3bfc75e6c9e58abc9d501318499c8d0abc212f57
“ame <1772702063@qq.com>
2019-05-27 14:38:02 +0800

    v

--- crontab/config/config.ini.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('InShopNC') or exit('Access Invalid!');
-//$config = array();
-$config['debug'] = false;
-return $config;
--- wap/js/tmpl/member_refund.js
+++
@@ -1,1 +0,0 @@
-$(function(){var e=getCookie("key");var t=new ncScrollLoad;t.loadInit({url:ApiUrl+"/index.php?act=member_refund&op=get_refund_list",getparam:{key:e},tmplid:"refund-list-tmpl",containerobj:$("#refund-list"),iIntervalId:true,data:{WapSiteUrl:WapSiteUrl}})});
